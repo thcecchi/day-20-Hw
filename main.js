49f96d215ab5875ddae07739ed95496f170e1ef7
Tyler Cecchi <thcecchi@gmail.com>
2015-02-10 17:24:51 -0500

    major design and functionality updates

--- main.js
+++ main.js
@@ -15,6 +15,60 @@ $(document).ready(function () {
 
     initStyle: function () {
       toDoList.renderAllListItems();
+      $('.completedTitle').hide()
+
+      completedArray=[]
+
+      // click check mark and move to completed tasks ///
+     //////////////////////////////////////////////
+     $('.container').on('click', '.fa-check-square', function (e) {
+       e.preventDefault
+
+       // show completed container if it contains a completed item ///
+      ///////////////////////////////////////////////////////////////
+      if ( $('.completed').has('.listItemCard') ) {
+        $('.completedTitle').show()
+      }
+
+      else {
+        $('.completedTitle').hide()
+      }
+
+      var detachedItem = $(this).parent('.listItemCard').detach();
+       detachedItem.appendTo('.completed').css('border', '2px dashed gainsboro');
+
+       // push completed task itemid to array ///
+      //////////////////////////////////////////
+       completedArray.push($(this).parent('.listItemCard').data('itemid'))
+       console.log(completedArray)
+
+      //  // set up ajax request to add key "completed" ///
+      // /////////////////////////////////////////////////
+      var itemId = $(this).closest('.listItemCard').data('itemid');
+
+      var addedListItem = {
+        completed: "Y"
+      }
+
+      toDoList.addedListItem(itemId, addedListItem);
+
+
+       // count number of completed tasks ///
+      //////////////////////////////////////
+
+      var completedCount = parseInt($(".completed > .listItemCard").length)
+
+      function completeNum(e){
+        completedCount ++;
+
+        document.getElementById('completedCount').innerHTML = completedCount-1;
+        return true
+      }
+
+      completeNum()
+
+    });
+
     },
 
     initEvents: function () {
@@ -34,12 +88,37 @@ $(document).ready(function () {
 
       // Delete current ////
       /////////////////////
-      $('.wrapper').on('click', '.delete', function (event) {
+      $('.wrapper').on('click', '.fa-times', function (event) {
         event.preventDefault();
 
-        var itemId = $('.listItemCard').data('itemid');
+        var itemId = $(this).closest('.listItemCard').data('itemid');
         console.log(itemId);
         toDoList.deleteListItem(itemId);
+        $(this).closest('.listItemCard').remove()
+
+        // show completed container if it contains a completed item ///
+       ///////////////////////////////////////////////////////////////
+        var completedCount = parseInt($(".completed > .listItemCard").length)
+        console.log(completedCount)
+
+        function completeNum(e){
+          completedCount ++;
+
+          document.getElementById('completedCount').innerHTML = completedCount-1;
+          return true
+        }
+
+        completeNum()
+
+        // show completed container if it contains a completed item ///
+       ///////////////////////////////////////////////////////////////
+       if ( $('.completed').has('.listItemCard') ) {
+         $('.completedTitle').show()
+       }
+
+       else {
+         $('.completedTitle').hide()
+       }
       });
 
       // Update current ////
@@ -47,16 +126,17 @@ $(document).ready(function () {
       $('.wrapper').on('dblclick', '.listItem', function (event) {
         event.preventDefault();
 
-        $(this).closest('.listItem').replaceWith('<input type="text" class="updateListItem" name="updateListItem"> value="<%= listItemText %>"</input>');
+        // replace text iwth input and add update button ////
+        ////////////////////////////////////////////////////
+        $(this).closest('.listItem').replaceWith('<input type="text" class="updateListItem" name="updateListItem"</input>');
 
-        var itemId = $('.listItemCard').data('itemid');
-
+        $('button.update').show()
       });
 
       $('.wrapper').on('click', '.update', function (event) {
         event.preventDefault();
 
-        var itemId = $('.listItemCard').data('itemid');
+        var itemId = $('.updateListItem').closest('.listItemCard').data('itemid');
 
         var editedListItem = {
           listItemText: $('.updateListItem').val()
@@ -82,12 +162,31 @@ $(document).ready(function () {
         success: function (items) {
 
           var compiledTemplate = _.template(templates.toDoList);
-          // $('.container').append(compiledTemplate);
+          // $('.container').append(compiledTemplate)
 
           var markup = "";
           items.forEach(function (item, idx, arr) {
             markup += compiledTemplate(item);
           });
+          newArr = []
+          items.forEach(function(item, idx, arr) {
+              // if (_.has(item, "completed")==true) {
+              //   item.detach();
+              //   item.appendTo('.completed').css('border', '2px dashed gainsboro');
+              // }
+            newArr.push(item)
+            console.log(newArr)
+          })
+
+          _.each(newArr, function (element, index, list) {
+            if (_.has(element, "completed")) {
+              element[index].detach();
+              element[index].appendTo('.completed').css('border', '2px dashed gainsboro');
+            }
+          })
+
+
+
           console.log('markup is.....', markup);
           $('.container').html(markup);
         },
@@ -97,6 +196,7 @@ $(document).ready(function () {
         }
 
       });
+
     },
 
     createListItem: function (items) {
@@ -142,6 +242,21 @@ $(document).ready(function () {
           success: function (data) {
             console.log(data);
             toDoList.renderAllListItems();
+          },
+          error: function (err) {
+            console.log(err);
+          }
+        });
+    },
+
+    addedListItem: function (id, items) {
+
+        $.ajax({
+          url: toDoList.config.url + '/' + id,
+          data: items,
+          type: 'PUT',
+          success: function (data) {
+            _.extend(id, data)
           },
           error: function (err) {
             console.log(err);
--- templates.js
+++ templates.js
@@ -1,11 +1,10 @@
 var templates = {};
 
 templates.toDoList = [
-"<div class='listItemCard' data-itemid='<%= _id %>'>",
-// "<i><%= check =></i>",
-// "<div class='listName'>",
-"<h3 class='listItem'><%= listItemText %></h3>",
-// "</div>",
-// "<i class='ex'><%= check =></i>",
+"<div class='listItemCard large-12 columns end' data-itemid='<%= _id %>'>",
+"<h5 class='listItem'><%= listItemText %></h5>",
+"<i class='fa fa-check-square'></i>",
+"<i class='fa fa-times'></i>",
+"<button class='large-1 columns update' name='update'>Update</button>",
 "</div>"
 ].join("")
